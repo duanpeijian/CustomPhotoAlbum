f7fb6add3283b37f4e007e9e72b4096504eca960
Donew-Mac <duanpeijian@outlook.com>
2016-06-20 19:29:55 +0800

    Initial Commit

---
+++ CustomPhotoAlbum/CustomPhotoAlbum.h
@@ -0,0 +1,13 @@
+//
+//  CustomPhotoAlbum.h
+//  CustomPhotoAlbum
+//
+//  Created by Donew on 16/6/20.
+//  Copyright © 2016年 Donew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CustomPhotoAlbum : NSObject
+
+@end
---
+++ CustomPhotoAlbum/CustomPhotoAlbum.m
@@ -0,0 +1,13 @@
+//
+//  CustomPhotoAlbum.m
+//  CustomPhotoAlbum
+//
+//  Created by Donew on 16/6/20.
+//  Copyright © 2016年 Donew. All rights reserved.
+//
+
+#import "CustomPhotoAlbum.h"
+
+@implementation CustomPhotoAlbum
+
+@end
